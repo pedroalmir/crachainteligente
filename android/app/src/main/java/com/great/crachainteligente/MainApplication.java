e3c784c0d58f5e8405c1bd3b62f081b9c6fe2827
Pedro Almir <petrus.cc@gmail.com>
2019-03-18 22:19:37 -0300

    Google Service Config

--- android/app/src/main/java/com/great/crachainteligente/MainApplication.java
+++ android/app/src/main/java/com/great/crachainteligente/MainApplication.java
@@ -1,4 +1,4 @@
-package com.invertase.rnfirebasestarter;
+package com.great.crachainteligente;
 
 import android.annotation.SuppressLint;
 import android.app.Application;
--- ios/CrachaInteligenteTests/CrachaInteligenteTests.m
+++ ios/CrachaInteligenteTests/CrachaInteligenteTests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface RNFirebaseStarterTests : XCTestCase
+@interface CrachaInteligenteTests : XCTestCase
 
 @end
 
-@implementation RNFirebaseStarterTests
+@implementation CrachaInteligenteTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
